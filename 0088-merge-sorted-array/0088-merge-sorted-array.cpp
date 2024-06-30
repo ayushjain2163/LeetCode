559cd20c0c3630eca33aeee8654e1a712d7e070c
Ayush Jain <97034183+ayushjain2163@users.noreply.github.com>
2024-06-30 17:15:40 +0530

    Time: 2 ms (49.6%), Space: 11 MB (32.59%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -3,7 +3,38 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
     
     
+    //     for(int i=0;i<n;i++){
+    //         nums1[i+m]=nums2[i];
+    //     }
+    //     sort(nums1.begin(),nums1.end());
 
+    // vector<int> temp;
+    // for(int i=0;i<m;i++){
+    //     temp.push_back(nums1[i]);
+    // }
+    // int i=0,j=0,x=0;
+    // while(i<m && j<n){
+    //     if(temp[i]<=nums2[j])
+    //     {
+    //         nums1[x++]=temp[i];
+    //         i++;
+    //     }
+    //     else{
+    //         nums1[x++]=nums2[j];
+    //         j++;
+    //     }
+    // }
+    // while(i<m){
+    //     nums1[x++]=temp[i];
+    //     i++;
+    // }
+    // while(j<n){
+    //     nums1[x++]=nums2[j];
+    //     j++;
+    // }
+
+
+    // Most optimized
     int i=m-1,j=n-1;
     int x=i+n;
 
