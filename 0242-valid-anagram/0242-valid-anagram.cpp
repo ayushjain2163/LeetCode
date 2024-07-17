7868160623a9a6e550d77f214d4357ecc18fff7e
Ayush Jain <97034183+ayushjain2163@users.noreply.github.com>
2024-07-17 18:18:41 +0530

    Time: 13 ms (26.1%), Space: 8.8 MB (44.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size())
+        if(s.length()!=t.length())
             return false;
+
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
 
-        for(int i=0;i<s.size();i++){
+        for(int i=0;i<s.length();i++){
             if(s[i]!=t[i])
                 return false;
-        }
+        }    
+
         return true;
     }
 };
