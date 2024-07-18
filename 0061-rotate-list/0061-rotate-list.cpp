bc05c57e3130b5a610d1eb960b3ebc6d0ec7986e
Ayush Jain <97034183+ayushjain2163@users.noreply.github.com>
2024-07-18 19:47:14 +0530

    Time: 7 ms (33.12%), Space: 16.5 MB (7.44%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -0,0 +1,74 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==NULL || head->next==NULL)
+            return head;
+        int n=0;
+        struct ListNode* temp=head;
+        while(temp!=NULL){
+            n++;
+            temp=temp->next;
+        }
+        // std::cout << n << std::endl;
+        if(head==NULL || head->next==NULL|| k%n==0)
+            return head;
+        k=k%n;
+        temp=head;
+        int i=1;
+        while(i<n-k){
+            temp=temp->next;
+            i++;
+        }
+        // std::cout << i << std::endl;
+        // std::cout << temp->val << std::endl;
+
+        struct ListNode* tail=temp;
+        struct ListNode* newHead=temp->next;
+        temp=newHead;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=head;
+        tail->next=NULL;
+        
+        
+        return newHead;
+    }
+};
+
+
+
+
+// class Solution {
+// public:
+//     ListNode* rotate(ListNode* head){
+//         struct ListNode* temp=head;
+//         while(temp->next->next!=NULL){
+//             temp=temp->next;
+//         }
+//         struct ListNode* last=temp;
+//         temp=temp->next;
+//         last->next=NULL;
+//         temp->next=head;
+//         return temp;
+//     }
+//     ListNode* rotateRight(ListNode* head, int k) {
+//         if(head==NULL || head->next==NULL)
+//             return head;
+//         while(k){
+//             head=rotate(head);
+//             k--;
+//         }
+//         return head;
+//     }
+// };
